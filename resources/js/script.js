99fcb8431744c50634b3a590e3d6a3fa3404bb03
Jan Radziminski <radziminski.j@gmail.com>
2019-11-29 19:41:56 +0100

    Initialization

---
+++ resources/js/script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function() {
+    console.log('hello');
+
+    // Stivy Nav Fade Animation
+    $('.js--section-about').waypoint(function(direction) {
+        if(direction == 'down') {
+            $('nav').addClass('sticky');
+            $('.sticky').removeClass('animated fadeOutUp');
+            $('.sticky').addClass('animated fadeInDown');
+            console.log('activated down');
+        } else {
+            $('.sticky').removeClass('animated fadeInDown');
+            $('.sticky').addClass('animated fadeOutUp');
+           
+            console.log('activated up');
+        }
+    }, {
+        offset: '65%'
+    });
+
+    $('header').waypoint(function(direction) {
+        if(direction == 'up') {
+            $('.sticky').removeClass('animated fadeOutUp');
+            $('nav').removeClass('sticky');
+            console.log('activated up removal');
+        }
+
+    }, {
+        offset: '-10%;'
+    });
+
+});
