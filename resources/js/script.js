24d3a6d039edf6d24c0def0deac8158028d5f555
Jan Radziminski <radziminski.j@gmail.com>
2019-11-30 15:15:32 +0100

    page load check upd

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,17 @@
 
 // Waiting for background Image
 var src = $('header').css('background-image');
-
 urlSrc = src.slice(56, src.length);
 var url = urlSrc.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
 var img = new Image();
 img.onload = function() {
     console.log('loadeda')
-    $('heading-primary--main').addClass('animateLeft');
-    $('heading-primary--sub').addClass('animateRight');
-    $('js--animate-1').addClass('animateBottom');
+    $('.heading-primary--main').addClass('animateLeft');
+    $('.heading-primary--main').css('opacity', 1);
+    $('.heading-primary--sub').addClass('animateRight');
+    $('.heading-primary--sub').css('opacity', 1);
+    $('.js--animate-1').addClass('animateBottom');
+    $('.js--animate-1').css('opacity', 1);
 }
 img.src = url;
 if (img.complete) img.onload();
