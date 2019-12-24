5838b3732c77eaa22f6c58682a56af330f6386fb
Jan Radziminski <radziminski.j@gmail.com>
2019-12-24 15:00:51 +0100

    pop up update

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
                     loader: 'file-loader',
                     options: {
                         name: '[name].[ext]',
-                        outputPath: './data/img',
+                        outputPath: './data',
                         esModule: false,
                     }
                 }]
